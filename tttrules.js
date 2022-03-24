897a35a02429f8e021d62defffe7ff5c1fd0ccab
Eliza Nicholls <eliza@Elizas-MacBook-Pro.local>
2022-03-24 18:11:11 +1100

    boxesVanish!

--- tttrules.js
+++ tttrules.js
@@ -1,6 +1,6 @@
 console.log('TTT RULES RULES!!')
 
-    let playerOne = true;
+let playerOne = true;
 
 
 const ticktackerButton = $('.ticktackers');
@@ -46,7 +46,7 @@ function updateValue(ticktackerButton) {
   
     if (playerOne && boxNotClicked){
   
-      ticktackerButton.innerText = 'x'
+      ticktackerButton.innerText = 'x';
       values[ticktackerButton.id] = 'x';
       playerOne = !playerOne;
       console.log(values);
@@ -71,14 +71,41 @@ const ticktackers = [...document.getElementsByClassName('ticktackers')];
 // console.log('------ ticktackers: ', ticktackers);
 
 function updateButtonValue() {
-  ticktackers.map((ticktackerButton) => {
-    ticktackerButton.addEventListener("click", () => updateValue(ticktackerButton));
-       ticktackerButton.addEventListener("click", () => console.log('I clicked: ', ticktackerButton.id));
+        ticktackers.map((ticktackerButton) => {
+        ticktackerButton.addEventListener("click", () => updateValue(ticktackerButton));
+        ticktackerButton.addEventListener("click", () => console.log('I clicked: ', ticktackerButton.id));
+//removeBox
+        document.getElementById('0').addEventListener("click", () => $('#zero').css('visibility', 'hidden'));
+        document.getElementById('1').addEventListener("click", () => $('#one').css('visibility', 'hidden'));
+        document.getElementById('2').addEventListener("click", () => $('#two').css('visibility', 'hidden'));
+        document.getElementById('3').addEventListener("click", () => $('#three').css('visibility', 'hidden'));
+        document.getElementById('4').addEventListener("click", () => $('#four').css('visibility', 'hidden'));
+        document.getElementById('5').addEventListener("click", () => $('#five').css('visibility', 'hidden'));
+        document.getElementById('6').addEventListener("click", () => $('#six').css('visibility', 'hidden'));
+        document.getElementById('7').addEventListener("click", () => $('#seven').css('visibility', 'hidden'));
+        document.getElementById('8').addEventListener("click", () => $('#eight').css('visibility', 'hidden'));
+//removeShadow
+        document.getElementById('0').addEventListener("click", () => $('#shadowZero').css('visibility', 'hidden'));
+        document.getElementById('1').addEventListener("click", () => $('#shadowOne').css('visibility', 'hidden'));
+        document.getElementById('2').addEventListener("click", () => $('#shadowTwo').css('visibility', 'hidden'));
+        document.getElementById('3').addEventListener("click", () => $('#shadowThree').css('visibility', 'hidden'));
+        document.getElementById('4').addEventListener("click", () => $('#shadowFour').css('visibility', 'hidden'));
+        document.getElementById('5').addEventListener("click", () => $('#shadowFive').css('visibility', 'hidden'));
+        document.getElementById('6').addEventListener("click", () => $('#shadowSix').css('visibility', 'hidden'));
+        document.getElementById('7').addEventListener("click", () => $('#shadowSeven').css('visibility', 'hidden'));
+        document.getElementById('8').addEventListener("click", () => $('#shadowEight').css('visibility', 'hidden'));
        
       
   
   })
 }
+
+let moBoxesMoShadows = function (){
+  //removeBox
+  $('.cube').css('visibility', 'hidden');
+  $('.cube_shadow').css({'visibility': 'hidden', 'transition': 'none'});
+};
+
 
 updateButtonValue();
 
@@ -101,27 +128,28 @@ const checkWin = function (){
     if (rowOne){
         // console.log(values[0] + ' wins!');
          $('.winner').css('visibility', 'visible');
+         moBoxesMoShadows();
       } else if (rowTwo) {
         $('.winner').css('visibility', 'visible');
-        // console.log(values[3] + ' wins!');
+        moBoxesMoShadows();
       } else if (rowThree){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[6] + ' wins!')
+        moBoxesMoShadows();
       }  else if (columnA){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[0] + ' wins!')
+        moBoxesMoShadows();
       }  else if (columnB){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[1] + ' wins!')
+        moBoxesMoShadows();
       }  else if (columnC){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[2] + ' wins!')
+        moBoxesMoShadows();
       }  else if (diagonalTop){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[0] + ' wins!')
+        moBoxesMoShadows();
       }  else if (diagonalBottom){
         $('.winner').css('visibility', 'visible');
-        // console.log(values[6] + ' wins!')
+        moBoxesMoShadows();
       } else if (draw());
     
   
